44df251ca0c54cea074158cad5bd920478d6982e
Vinay Prajapati <vinay.prajapati@intelligrape.com>
2017-08-27 13:32:56 +0530

    Added initial set of example code

---
+++ public/index.jsx
@@ -0,0 +1,9 @@
+/**
+ * Created by vinayprajapati on 27/08/17.
+ */
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Greeter from './components/Greeter.jsx';
+
+ReactDOM.render(<Greeter />, document.getElementById('root'));
---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+
+const PORT = process.env.PORT || 3000;
+app.use(express.static('dist'));
+app.use(function (req, res, next){
+    if (req.headers['x-forwarded-proto'] === 'http') {
+        next();
+    } else {
+        res.redirect('http://' + req.hostname + req.url);
+    }
+});
+app.listen(PORT, function () {
+    console.log("Express server is up on port no. - " + PORT);
+})
